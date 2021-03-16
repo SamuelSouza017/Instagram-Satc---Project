5c4c201ccd5f8a52bb2e784408dfd1e2aaeea68b
SamuelSouza017 <79282222+SamuelSouza017@users.noreply.github.com>
2021-03-15 21:28:29 -0300

    Add files via upload

---
+++ src/chat/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   
+<div className="tudo">
+
+<div className="dados">
+<div className="foto1">
+   <div className="circulo1"></div>
+</div>
+
+<div className="dadospessoa1">
+      <spam className="nome1">PESSOA 01</spam>
+      <spam className="online1">ONLINE</spam>
+</div>
+
+<div className="foto2">
+   <div className="circulo2"></div>
+</div>
+
+<div className="dadospessoal2">
+      <spam className="nome2">PESSOA 02</spam>
+      <spam className="online2">OFFLINE</spam>
+</div>
+</div>
+
+<div className="nomeconversa">
+  
+<div className="nomepessoa">
+   <div className="fotogrupo"></div>
+<spam className="nomegrupo">PESSOA 02</spam>
+
+</div>
+
+<div className="escrever">
+   <input className="espaçoescrever" type="text">
+   </input>
+   <input className="enviar" type="button" value="ENVIAR"></input>
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+</div>    
+
+  );
+}
+
+export default App;
